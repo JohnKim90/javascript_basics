b41acd09537be31918918149429ece26269f69f8
johnkimaiyo <kimaiyojohn6@gmail.com>
2022-08-07 14:36:51 +0300

    javascript objects props

---
+++ objects/object accessories/objectaccessories.js
@@ -0,0 +1,20 @@
+let profile ={
+    born:"30 August 1930",
+    univerisy:"Unversity of Pennyslvania",
+    occupation:"Investor",
+    yearsactive:"since 1951",
+    company:"Berkshire Hathaway",
+    politicalpart:"Democratic",
+    spouse:"Susan Thomson",
+    children:"Susan Alice Buffet , Howard Graham BUffet, Peter BUffet",
+    parents:"Howard Buffet , Leila Stahi Buffet",
+}
+console.log(profile.born);
+console.log(profile.univerisy);
+console.log(profile.occupation);
+console.log(profile.yearsactive);
+console.log(profile.company);
+console.log(profile.politicalpart);
+console.log(profile.spouse);
+console.log(profile.children);
+console.log(profile.parents);
