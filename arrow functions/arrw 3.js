e9fe8be5d2f4c69be66641a4cf2d06609610825d
johnkimaiyo <kimaiyojohn6@gmail.com>
2022-08-15 13:24:54 +0300

    sorting array descending order

---
+++ arrow functions/arrw 3.js
@@ -0,0 +1,70 @@
+//exampe 1
+let numbers = [4, 2, 6];
+numbers.sort(function (a, b) {
+  return b - a;
+});
+console.log(numbers);
+
+// example 2
+let onenumbers = [21, 41, 61];
+onenumbers.sort(function (c, d) {
+  return d - c;
+});
+console.log(onenumbers);
+
+// example 3
+let twonumbers = [22, 42, 62];
+twonumbers.sort(function (e, f) {
+  return f - e;
+});
+console.log(twonumbers);
+
+// example 4
+let threenumbers = [23, 43, 63];
+threenumbers.sort(function (g, h) {
+  return h - g;
+});
+console.log(threenumbers);
+
+// example 5
+let fournumbers = [24,44,64];
+fournumbers.sort(function(k,l){
+    return l-k;
+});
+console.log(fournumbers);
+
+// example 6
+let fivenumbers = [25,45,65];
+fivenumbers.sort(function(m,n){
+    return n-m;
+});
+console.log(fivenumbers);
+
+// example 7
+let sixnumbers = [26,46,66];
+sixnumbers.sort(function (o,p){
+    return p-o;
+});
+console.log(sixnumbers);
+
+// example 8
+let sevennumbers = [27,47,67];
+sevennumbers.sort(function(q,r){
+    return r-q;
+});
+console.log(sevennumbers);
+
+// exampe 9s
+let eightnumbers = [28,48,68];
+eightnumbers.sort(function(s,t){
+    return t-s;
+});
+console.log(eightnumbers);
+
+// example 10
+let ninenumbers = [29,49,69];
+ninenumbers.sort(function(u,v){
+    return v-u;
+});
+console.log(ninenumbers);
+
