486b41f665fa8321978b20e7b0883cce9ba707a7
johnkimaiyo <kimaiyojohn6@gmail.com>
2022-08-15 12:35:18 +0300

    arrow functions

---
+++ arrow functions/arrw 2.js
@@ -0,0 +1,41 @@
+// examplbe 1
+
+let onearrowadd = (a, b) => a + b;
+console.log(add(10, 20)); //
+
+// example 2
+
+let twoarrowadd = (c, d) => c + d;
+console.log(add(20, 30));
+
+// example 3
+let threearrowadd = (e, f) => e + f;
+console.log(add(30, 40));
+
+// example 4
+let fourarrowadd = (g, h) => g + h;
+console.log(add(40, 50));
+
+// example 5
+let fivearrowadd = (i, j) => i + j;
+console.log(add(50, 60));
+
+// example 6
+let sixarrowadd = (k, l) => k + l;
+console.log(add(60, 70));
+
+// example 7
+let sevenarrowadd = (m, n) => m + n;
+console.log(add(70, 80));
+
+// example 8
+let eightarrowadd = (o, p) => o + p;
+console.log(add(80, 90));
+
+// example 9
+let ninearrowadd = (q, r) => q + r;
+console.log(add(90, 100));
+
+// example 10
+let tenarrowadd = (s, t) => s + t;
+console.log(add(100, 120));
