abc36ecc6791c316d4d3784043855e7d3c78240e
johnkimaiyo <kimaiyojohn6@gmail.com>
2022-08-15 14:23:18 +0300

    map length

--- arrow functions/arrw 3.js
+++ arrow functions/arrw 3.js
@@ -1,70 +1,49 @@
 //exampe 1
 let numbers = [4, 2, 6];
-numbers.sort(function (a, b) {
-  return b - a;
-});
+numbers.sort((a, b) => b - a);
 console.log(numbers);
 
 // example 2
 let onenumbers = [21, 41, 61];
-onenumbers.sort(function (c, d) {
-  return d - c;
-});
+onenumbers.sort((c, d) => d - c);
 console.log(onenumbers);
 
 // example 3
 let twonumbers = [22, 42, 62];
-twonumbers.sort(function (e, f) {
-  return f - e;
-});
+twonumbers.sort((e, f) => f - e);
 console.log(twonumbers);
 
 // example 4
 let threenumbers = [23, 43, 63];
-threenumbers.sort(function (g, h) {
-  return h - g;
-});
+threenumbers.sort((g, h) => h - g);
 console.log(threenumbers);
 
 // example 5
-let fournumbers = [24,44,64];
-fournumbers.sort(function(k,l){
-    return l-k;
-});
+let fournumbers = [24, 44, 64];
+fournumbers.sort((k, l) => l - k);
 console.log(fournumbers);
 
 // example 6
-let fivenumbers = [25,45,65];
-fivenumbers.sort(function(m,n){
-    return n-m;
-});
+let fivenumbers = [25, 45, 65];
+fivenumbers.sort((m, n) => n - m);
 console.log(fivenumbers);
 
 // example 7
-let sixnumbers = [26,46,66];
-sixnumbers.sort(function (o,p){
-    return p-o;
-});
+let sixnumbers = [26, 46, 66];
+sixnumbers.sort((o, p) => p - o);
 console.log(sixnumbers);
 
 // example 8
-let sevennumbers = [27,47,67];
-sevennumbers.sort(function(q,r){
-    return r-q;
-});
+let sevennumbers = [27, 47, 67];
+sevennumbers.sort((q, r) => r - q);
 console.log(sevennumbers);
 
 // exampe 9s
-let eightnumbers = [28,48,68];
-eightnumbers.sort(function(s,t){
-    return t-s;
-});
+let eightnumbers = [28, 48, 68];
+eightnumbers.sort((s, t) => t - s);
 console.log(eightnumbers);
 
 // example 10
-let ninenumbers = [29,49,69];
-ninenumbers.sort(function(u,v){
-    return v-u;
-});
+let ninenumbers = [29, 49, 69];
+ninenumbers.sort((u, v) => v - u);
 console.log(ninenumbers);
-
---
+++ arrow functions/arrw 4.js
@@ -0,0 +1,50 @@
+// example 1
+let names = ["john", "mac", "peter"];
+let length = names.map((name) => name.length);
+console.log(length);
+
+// exampe 2
+let months = ["jan", "feb", "mar"];
+let monthslength = months.map((months) => months.length);
+console.log(monthslength);
+
+// example 3
+let countries = ["kenya", "uganda", "tanzania"];
+let countrieslength = countries.map((countries) => countries.length);
+console.log(countrieslength);
+
+// example 4
+let cities = ["nairobi", "kampala", "mwanzo"];
+let citieslength = cities.map((cities) => cities.length);
+console.log(citieslength);
+
+// example 5
+let football = ["manu", "mancity", "liverpool"];
+let footballlength = football.map((football) => football.length);
+console.log(footballlength);
+
+// example 6
+
+let car = ["toyota", "mazda", "bmw"];
+let carlength = car.map((car) => car.length);
+console.log(carlength);
+
+// example 7
+let usa = ["newyork", "chicago", "texs"];
+let usalength = usa.map((usa) => usa.length);
+console.log(carlength);
+
+// examplle 8
+let basketball = ["lakers", "rockets", "celtics"];
+let basketballlength = basketball.map((basketball) => basketball.length);
+console.log(basketballlength);
+
+// example 9
+let career = ['lawyer','doctor','accountant'];
+let careerlength = career.map((career)=>career.length);
+console.log(careerlength);
+
+// example 10
+let billionaire = ['buffet','musk','bazos'];
+let billionairelength = billionaire.map((billionaire)=>billionaire.length);
+console.log(billionairelength);
